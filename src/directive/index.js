c8abcf0d0c75f34c086f97ab349a95d27ce4c059
tuanmjnh <minhtuan200990@gmail.com>
2019-08-02 22:06:29 +0700

    tm

--- src/directive/index.js
+++ src/directive/index.js
@@ -1,12 +1,40 @@
-export function trim (el, binding, vnode) {
-  if (binding.value) {
-    el.value = 'trimed' // binding.value
-    console.log(el.value)
+import Vue from 'vue'
+Vue.directive('trim', {
+  bind(el, binding, vnode) {
+    console.log(el.contains(event.target))
+    console.log(vnode)
+    console.log(binding)
+    // Vue.set(vnode.context, vnode.data.model.expression, 'tm')
+    vnode.context.$emit('update:' + vnode.data.model.expression, 'tm')
+  },
+  updated: function(el, binding, vnode) {
+
   }
-  // console.log(el, binding, vnode)
-  // vnode.data.model.value = 'trimed'
-  // el.innerHTML = 'trimed'
-}
+})
+// export function trim (el, binding, vnode) {
+//   if (binding.value) {
+//     el.value = 'trimed' // binding.value
+//     console.log(binding.name, binding.value)
+//   }
+//   // console.log(el, binding, vnode)
+//   // vnode.data.model.value = 'trimed'
+//   // el.innerHTML = 'trimed'
+// }
+
+// export const trim = {
+//   update: (el, binding, vnode, oldVnode, vModel) => {
+//     console.log(vnode, el.value)
+//     console.log(vnode.data.model.expression.split('.'))
+//     // vnode.context.user.displayName = 'tm'
+//     // vnode.context.set(vnode.data.model.expression, 'tm')
+//     // Vue.set(vnode.context, binding.expression, date);
+//     if (binding.value) {
+//       el.value = 'trimed' // binding.value
+//     }
+//   }
+//   // vnode.data.model.value = 'trimed'
+//   // el.innerHTML = 'trimed'
+// }
 
 // Vue.directive("trim", {
 //   inserted: function(el) {
